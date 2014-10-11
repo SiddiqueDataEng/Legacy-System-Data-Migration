eeb5cb107b78c5b0d0e848a301567f2e9c5d680d
Muhammad Siddique <siddique@dataeng.com>
2014-10-11 17:08:00 +0500

    Implement triggers and data validation

---
+++ database/05-SeedData.sql
@@ -0,0 +1,19 @@
+/*
+ * Legacy System Data Migration
+ * Sample Data
+ * SQL Server 2008/2012
+ */
+
+USE MigrationDB;
+GO
+
+-- Insert sample records
+INSERT INTO dbo.MainRecords (RecordCode, RecordName, Description, CreatedBy)
+VALUES
+('REC001', 'Sample Record 1', 'First sample record for Legacy-System-Data-Migration', 'admin'),
+('REC002', 'Sample Record 2', 'Second sample record for Legacy-System-Data-Migration', 'admin'),
+('REC003', 'Sample Record 3', 'Third sample record for Legacy-System-Data-Migration', 'admin');
+
+PRINT 'Sample data inserted successfully';
+PRINT 'Total records: ' + CAST(@@ROWCOUNT AS VARCHAR(10));
+GO
